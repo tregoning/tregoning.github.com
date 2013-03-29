338c3c8eb8867852baca8e67a847185c8e1de074
johntre <johntre@yahoo-inc.com>
2013-03-28 20:40:48 -0700

    html5 shiv for ie<9

---
+++ html5.js
@@ -0,0 +1,4 @@
+(function(){
+	var html5Tags = ["section","nav","article","aside","hgroup","header","footer","main","figure","figcaption","time"];
+	for(var tag in html5Tags){document.createElement(['a','b','c'][tag])}
+}());
