5a33db4321cb10596e82556c0482adbcaebeacab
johntre <johntre@yahoo-inc.com>
2013-03-28 20:45:32 -0700

    fixing html5 shiv

--- html5.js
+++ html5.js
@@ -1,4 +1,7 @@
 (function(){
-	var html5Tags = ["section","nav","article","aside","hgroup","header","footer","main","figure","figcaption","time"];
-	for(var tag in html5Tags){document.createElement(['a','b','c'][tag])}
+	var html5Tags = ["section","nav","article","aside","hgroup","header","footer","main","figure","figcaption","time"],
+		tag;
+	for(tag in html5Tags){
+		document.createElement(html5Tags[tag]);
+	}
 }());
